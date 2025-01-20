1fce7f661ce935fb138b25ffda8bf8380a00f911
Kacper Bujak <kaccpper2@gmail.com>
2025-01-20 13:21:13 +0100

    LocalStrategy and JWTStrategy

---
+++ auth/auth.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const passportJWT = require('passport-jwt');
+const LocalStrategy = require('passport-local').Strategy;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(
+  new LocalStrategy( async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+
+      if (!user) {
+        return done(null, false, { message: 'User not found.' });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        return done (null, false, { message: 'Incorrect password.' });
+      }
+
+      return done (null, user);
+    }
+    catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+  },
+  async (token, done) => {
+    try {
+      return done(null, token);
+    }
+    catch (err) {
+      return done(err);
+    }
+  }
+));
